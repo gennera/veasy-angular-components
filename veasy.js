4770023710a382a94a98373aeab752c8a0cda8f3
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2019-02-19 12:10:03 -0300

    adding veasy-table module and directive

--- veasy.js
+++ veasy.js
@@ -1,3 +1,4 @@
 angular.module('veasy', []);
 angular.module('veasy.momentFormat', []);
 angular.module('veasy.calendar', [ 'veasy.momentFormat' ]);
+angular.module('veasy.table', [ 'ngSanitize', 'veasy.table.templates' ]);

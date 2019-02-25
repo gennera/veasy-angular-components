7f2830d6fa722c2ce79dd3a6a8ba0d8f070aaedc
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2019-02-25 15:01:49 -0300

    Fixes

--- demos/veasy-calendar/example.js
+++ demos/veasy-calendar/example.js
@@ -1,13 +1,9 @@
-angular.module('example', [
-  'veasy.momentFormat',
-  'veasy.calendar'
-])
+angular.module('example', ['veasy.momentFormat', 'veasy.calendar'])
 
 .controller('vMomentFormatController', ['$scope', function ($scope) {
   $scope.save = function (inputValue) {
     alert('Input value: ' + inputValue.format('DD/MM/YYYY HH:mm'));
   };
-
   $scope.$on('veasyMomentFormat:onChange', function (event, data) {
     console.log('veasyMomentFormat:onChange', data);
   });
@@ -33,60 +29,32 @@ angular.module('example', [
   const getEvents = function () {
     $timeout(function () {
       $scope.vCalendarConfig.events = [
-        { id: 1, title: 'State University of New York College of Agriculture and Technology at Cobleskill', startDate: '2018-07-17T23:40:00.000Z' },
-        { id: 2, title: 'Cardinal Stefan Wyszynski University in Warsaw', startDate: '2018-07-21T05:12:00.000Z' },
-        { id: 3, title: 'Vesalius College', startDate: '2018-05-21T03:19:00.000Z' },
-        { id: 4, title: 'University of Texas Health Center at Tyler', startDate: '2018-12-17T12:50:00.000Z' },
-        { id: 5, title: 'Buena Vista University', startDate: '2018-01-12T04:14:00.000Z', color: '#b75ece' },
-        { id: 6, title: 'University of Sunderland', startDate: '2018-11-16T22:04:00.000Z' },
-        { id: 7, title: 'Sinte Gleska University', startDate: '2018-01-17T22:01:00.000Z', color: '#840312' },
-        { id: 8, title: 'Universidad Pontificia de Salamanca', startDate: '2018-01-21T01:58:00.000Z' },
-        { id: 9, title: 'Yantai Education Institute & Yantai Television University', startDate: '2018-07-16T09:57:00.000Z' },
-        { id: 10, title: 'University of South Africa', startDate: '2018-01-11T05:28:00.000Z', color: '#8e9b30' },
-        { id: 11, title: 'Loyola University New Orleans', startDate: '2018-11-04T01:59:00.000Z' },
-        { id: 12, title: 'Osmangazi University', startDate: '2018-09-10T16:32:00.000Z', color: '#01b6e6' },
-        { id: 13, title: 'COSMIQ Institute of Technology', startDate: '2018-11-03T18:21:00.000Z' },
-        { id: 14, title: 'Nova Scotia College of Art and Design', startDate: '2018-01-24T13:53:00.000Z', color: '#3d59fe' },
-        { id: 15, title: 'University of Fine Arts Belgrade', startDate: '2018-02-07T13:12:00.000Z' },
-        { id: 16, title: 'Universidad de Valparaiso', startDate: '2018-10-14T18:39:00.000Z' },
-        { id: 17, title: 'Coleman College', startDate: '2018-07-11T08:42:00.000Z', color: '#b7a6fe' },
-        { id: 18, title: 'Walden University', startDate: '2018-12-28T16:12:00.000Z' },
-        { id: 18, title: 'Walden University', startDate: '2018-12-28T16:12:00.000Z' },
-        { id: 18, title: 'Walden University', startDate: '2018-12-28T16:12:00.000Z' },
-        { id: 18, title: 'Walden University', startDate: '2018-12-28T16:12:00.000Z' },
-        { id: 18, title: 'Walden University', startDate: '2018-12-28T16:12:00.000Z' },
-        { id: 19, title: 'The Robert Gordon University', startDate: '2018-08-11T18:44:00.000Z', color: '#8a6eab' },
-        { id: 20, title: 'Mie University', startDate: '2018-01-17T11:05:00.000Z' },
-        { id: 21, title: 'School of Management Fribourg', startDate: '2018-10-22T04:01:00.000Z' },
-        { id: 22, title: 'University of Rousse', startDate: '2018-05-07T20:43:00.000Z' },
-        { id: 23, title: 'Fachhochschule Stralsund', startDate: '2018-05-29T15:29:00.000Z', color: '#3eee24' },
-        { id: 24, title: 'National Chung Hsing University,  Taichung', startDate: '2018-06-20T04:22:00.000Z' },
-        { id: 25, title: 'Norwegian Teacher Academy for Studies in Religion and Education', startDate: '2018-08-03T04:24:00.000Z' },
-        { id: 26, title: 'Umutara Polytechnic', startDate: '2018-04-27T15:13:00.000Z' },
-        { id: 27, title: 'State University of New York College of Environmental Science and Forestry', startDate: '2018-09-26T21:42:00.000Z' },
-        { id: 28, title: 'Pandit Ravi Shankar Shukla University', startDate: '2018-09-30T21:30:00.000Z' },
-        { id: 29, title: 'Nnamdi Azikiwe University', startDate: '2018-12-30T18:55:00.000Z' },
-        { id: 30, title: 'Université Libre de Tunis', startDate: '2018-01-21T12:42:00.000Z' },
-        { id: 31, title: 'Southwest Missouri State University - West Plains', startDate: '2018-09-13T12:00:00.000Z', color: '#f739ef' },
-        { id: 32, title: 'Mazandaran University of Iran', startDate: '2018-03-20T17:35:00.000Z' },
-        { id: 33, title: 'Universidad Nacional Agraria La Molina', startDate: '2018-09-21T15:30:00.000Z' },
-        { id: 34, title: 'Royal College of Art', startDate: '2018-10-01T16:58:00.000Z' },
-        { id: 35, title: 'Medical College of Pennsylvania and Hahnemann University', startDate: '2018-11-04T16:34:00.000Z' },
-        { id: 36, title: 'Sage Graduate School', startDate: '2018-09-27T10:53:00.000Z' },
-        { id: 37, title: 'Sheridan College', startDate: '2018-08-20T03:42:00.000Z' },
-        { id: 38, title: 'University of Sunderland', startDate: '2018-05-08T17:44:00.000Z' },
-        { id: 39, title: 'University of Wales,  Aberystwyth', startDate: '2018-12-18T15:36:00.000Z', color: '#f21746' },
-        { id: 40, title: 'University of NorthWest', startDate: '2018-05-25T13:46:00.000Z' },
-        { id: 41, title: 'Chhatrapati Shahu Ji Maharaj University', startDate: '2018-07-07T06:48:00.000Z' },
-        { id: 42, title: 'Baku State University', startDate: '2018-05-05T23:00:00.000Z' },
-        { id: 43, title: 'Simmons College', startDate: '2018-06-07T15:56:00.000Z' },
-        { id: 44, title: 'Kanagawa University', startDate: '2018-08-12T22:22:00.000Z' },
-        { id: 45, title: 'Hokkaido University', startDate: '2018-06-01T04:07:00.000Z', color: '#e3dd6e' },
-        { id: 46, title: 'Fachhochschule Münster', startDate: '2018-07-28T15:07:00.000Z', color: '#9d52ed' },
-        { id: 47, title: 'Universidad Andina Simón Bolivar', startDate: '2018-03-22T11:14:00.000Z', color: '#ba99c8' },
-        { id: 48, title: 'University of Nebraska - Omaha', startDate: '2018-04-10T04:49:00.000Z' },
-        { id: 49, title: 'Tripura University', startDate: '2018-10-14T18:46:00.000Z', color: '#51d603' },
-        { id: 50, title: 'Universität Siegen', startDate: '2018-07-24T06:30:00.000Z' }
+        { id: 1, title: 'State University of New York College of Agriculture and Technology at Cobleskill', startDate: '2018-07-17T23:40:00.000Z', endDate: '2018-07-17T00:40:00.000Z' },
+        { id: 2, title: 'Cardinal Stefan Wyszynski University in Warsaw', startDate: '2018-07-21T05:12:00.000Z', endDate: '2018-07-21T06:12:00.000Z' },
+        { id: 3, title: 'Vesalius College', startDate: '2018-05-21T03:19:00.000Z', endDate: '2018-05-21T05:19:00.000Z' },
+        { id: 4, title: 'University of Texas Health Center at Tyler', startDate: '2018-12-17T12:50:00.000Z', endDate: '2018-12-17T13:50:00.000Z' },
+        { id: 5, title: 'Buena Vista University', startDate: '2018-01-12T04:14:00.000Z', endDate: '2018-01-12T05:14:00.000Z', color: '#b75ece' },
+        { id: 6, title: 'University of Sunderland', startDate: '2018-11-16T22:04:00.000Z', endDate: '2018-11-16T23:04:00.000Z' },
+        { id: 7, title: 'Sinte Gleska University', startDate: '2018-01-17T22:01:00.000Z', endDate: '2018-01-17T23:01:00.000Z', color: '#840312' },
+        { id: 8, title: 'Universidad Pontificia de Salamanca', startDate: '2018-01-21T01:58:00.000Z', endDate: '2018-01-21T02:58:00.000Z' },
+        { id: 9, title: 'Yantai Education Institute & Yantai Television University', startDate: '2018-07-16T09:57:00.000Z', endDate: '2018-07-16T10:57:00.000Z' },
+        { id: 10, title: 'University of South Africa', startDate: '2018-01-11T05:28:00.000Z', endDate: '2018-01-11T06:28:00.000Z', color: '#8e9b30' },
+        { id: 11, title: 'Loyola University New Orleans', startDate: '2018-11-04T01:59:00.000Z', endDate: '2018-11-05T01:59:00.000Z' },
+        { id: 12, title: 'Osmangazi University', startDate: '2018-09-10T16:32:00.000Z', endDate: '2018-09-10T17:32:00.000Z', color: '#01b6e6' },
+        { id: 13, title: 'COSMIQ Institute of Technology', startDate: '2018-11-03T18:21:00.000Z', endDate: '2018-11-03T19:21:00.000Z' },
+        { id: 14, title: 'Nova Scotia College of Art and Design', startDate: '2018-01-24T13:53:00.000Z', endDate: '2018-01-24T14:53:00.000Z', color: '#3d59fe' },
+        { id: 15, title: 'University of Fine Arts Belgrade', startDate: '2018-02-07T13:12:00.000Z', endDate: '2018-02-07T14:12:00.000Z' },
+        { id: 16, title: 'Universidad de Valparaiso', startDate: '2018-10-14T18:39:00.000Z', endDate: '2018-10-14T19:39:00.000Z' },
+        { id: 17, title: 'Coleman College', startDate: '2018-07-11T08:42:00.000Z', endDate: '2018-07-11T09:42:00.000Z', color: '#b7a6fe' },
+        { id: 18, title: 'Walden University', startDate: '2018-12-28T13:13:00.000Z', endDate: '2018-12-28T14:13:00.000Z' },
+        { id: 19, title: 'Walden University', startDate: '2018-12-28T12:12:00.000Z', endDate: '2018-12-28T13:12:00.000Z' },
+        { id: 20, title: 'Walden University', startDate: '2018-12-28T11:11:00.000Z', endDate: '2018-12-28T12:11:00.000Z' },
+        { id: 21, title: 'Walden University', startDate: '2018-12-28T14:14:00.000Z', endDate: '2018-12-28T15:14:00.000Z' },
+        { id: 22, title: 'Walden University', startDate: '2018-12-28T16:16:00.000Z', endDate: '2018-12-28T17:16:00.000Z' },
+        { id: 23, title: 'The Robert Gordon University', startDate: '2018-08-11T18:44:00.000Z', endDate: '2018-08-11T19:44:00.000Z', color: '#8a6eab' },
+        { id: 24, title: 'Mie University', startDate: '2018-01-17T11:05:00.000Z', endDate: '2018-01-17T12:05:00.000Z' },
+        { id: 25, title: 'School of Management Fribourg', startDate: '2018-10-22T04:01:00.000Z', endDate: '2018-10-22T05:01:00.000Z' },
+        { id: 26, title: 'University of Rousse', startDate: '2018-05-07T20:43:00.000Z', endDate: '2018-05-07T21:43:00.000Z' }
       ];
     }, 1000);
   }
